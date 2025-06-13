5502aebac819dcc64969ab2dc023504988bbf1e4
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2025-06-13 22:36:09 +0530

    minor error

--- src/components/student/JobSearch.tsx
+++ src/components/student/JobSearch.tsx
@@ -218,7 +218,7 @@ export default function JobSearch({
                     isApplied={appliedJobs.includes(job.id)}
                     onApply={() => onApply(job.id)}
                     index={index}
-                    isExpired={true}
+                    isExpiredIST={true}
                   />
                 ))}
               </div>
